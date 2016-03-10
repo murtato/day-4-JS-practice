99e5c4bdbd780fa82f78a11bfe0151885080d905
Victor Tran <murtato@gmail.com>
2016-03-10 13:08:14 -0800

    Progress so far on the JS day 4 lab

---
+++ fibonacci.js
@@ -0,0 +1,88 @@
+//ascii triangle
+function triangle(n) {
+  if(n<1){
+    return(n-1);
+  } else{
+
+  }
+}
+console.log(triangle(10))
+
+
+
+for (i=0; i < 20; i++)
+var fib = [0,1]
+
+for(var i=fib.length; i<=20; i++) {
+  // Each subsequent number in sequence  is the sum of the previous two numbers. Find first 20 numbers in fibonacci sequence. If next fibonacci number = previous + one before previous, then formula appears to be some form of (i-2)+(i-1)//
+  fib[i] = fib[i-2] + fib[i-1];
+  console.log(fib);
+}
+
+
+function fibonacci(n) {
+  if(n<2) {
+    return (n-1);
+  } else{
+    return fibonacci(n-2) + fibonacci(n-1);
+  }
+}
+console.log(fibonacci(20));
+//returns 20?
+
+for (i=0; i<=20; i++)
+  var fib = [0, 0]
+
+
+
+var bicycle = {numGears = 3};
+  numGears.getGears
+
+
+var person = {name: "Bob", age: 23};
+if(person.hasOwnProperty("name")){
+  alert("name is a property defined on this object")
+}
+
+var object = {Array}
+if(object.Array([Alice, Bob, Charlie])){
+  alert
+}
+
+car = {
+  smell: "Good"
+}
+
+var object = {}
+
+
+var booleanValue    = true;
+var numberValue     = 0;
+var stringValue     = 'null';
+var nullValue       = null;
+var undefinedValue  = 6364860684635/0;
+var undefinedValue2 = x;
+
+console.log (typeof(booleanValue));
+console.log (typeof(numberValue));
+console.log (typeof(stringValue));
+console.log (typeof(nullValue));
+console.log (typeof(undefinedValue));
+console.log (typeof(undefinedValue2));
+
+
+var attendees = ['Mia', 'Albert', 'Tony'];
+attendees = Array ('Mia', 'Albert', 'Tony');
+
+var attendee1 = {name: "Mia"}
+var attendee2 = {name: "Albert"}
+var attendee3 = {name: "Tony"}
+var arr=[];
+
+arr[0] = attendee1.name;
+arr[1] = attendee2.name;
+arr[2] = attendee3.name;
+
+console.log(arr)
+
+arr.unshift(Elaine)
